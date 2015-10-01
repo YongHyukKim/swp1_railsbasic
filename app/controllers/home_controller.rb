b6bae95906b0e13f0546d5dc02b14363f66a3400
yonghyukkim <yonghyuk0@gmail.com>
2015-10-01 05:58:51 +0000

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+    def index
+    end
+    
+    def about
+    end
+    
+    def first
+    end
+
+    def second
+    end
+    
+    def third
+    end
+    
+end
